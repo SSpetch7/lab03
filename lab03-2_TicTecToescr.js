501dcc01e0b1ae74c7878a110ca15da930cc9cd9
Kittinan Samakpong <joker.use.3@gmail.com>
2021-02-15 21:26:22 +0700

    con

--- lab03-2_TicTecToescr.js
+++ lab03-2_TicTecToescr.js
@@ -1,9 +1,15 @@
-function pick(){
+var board=["","","","","","","","",""]
+function mepick(){
     this.src='x.jpg';
-    
 }
 const image = document.querySelectorAll('img');
 for (var i = 0; i < image.length; i++) {
-    image[i].addEventListener('click', pick);
-    
+    image[i].addEventListener('click', mepick);
+    for(var k=0 ; k<=i ; k++){
+        
+    }
+}
+
+function compic(){
+
 }
