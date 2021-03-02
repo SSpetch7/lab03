5a7c47848f066fae61aabff937d1d45df5999f3d
Kittinan Samakpong <joker.use.3@gmail.com>
2021-03-03 01:29:46 +0700

    anser

---
+++ lab03-1-anser.js
@@ -0,0 +1,4 @@
+let character = prompt("What is your favorite charactor?");
+console.log(character + " is your favorite charactor.");
+let color_c = prompt("What is the main color of "+ character +"?");
+console.log(character+" is "+color_c+".");
